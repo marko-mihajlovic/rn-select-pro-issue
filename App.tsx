1da98015373805d32596378b57789deef1c51bdb
Marko Mihajlovic <mmaker.biz@gmail.com>
2024-07-06 19:56:52 +0200

    add @mobile-reality/react-native-select-pro

--- App.tsx
+++ App.tsx
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Select,
+  SelectProvider,
+} from "@mobile-reality/react-native-select-pro";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SelectProvider>
+        <SafeAreaView style={styles.container}>
+          <Select options={data} />
+        </SafeAreaView>
+      </SelectProvider>
+    </SafeAreaProvider>
   );
 }
+
+const data = [
+  {
+    label: "Option 1",
+    value: "option1",
+  },
+  {
+    label: "Option 2",
+    value: "option2",
+  },
+  {
+    label: "Option 3",
+    value: "option3",
+  },
+  {
+    label: "Option 4",
+    value: "option4",
+  },
+];
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    margin: 24,
+    justifyContent: "center",
   },
 });
